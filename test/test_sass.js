90b93f9630f3c2e153acbb31b2ea5f8541a5f019
Claire Parker <claire.parker87@gmail.com>
2017-03-29 22:19:20 +0100

    :whale: Refactor test structure

--- test/test_sass.js
+++ test/test_sass.js
@@ -2,5 +2,5 @@
 var path = require('path');
 var sassTrue = require('sass-true');
 
-var sassFile = path.join(__dirname, 'mixinTests.scss');
+var sassFile = path.join(__dirname, 'tests.scss');
 sassTrue.runSass({file: sassFile}, describe, it);
